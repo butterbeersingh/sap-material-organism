063cecd05a1491fa85a4daee47420cfe18739241
butterbeersingh <gurvinders1118@gmail.com>
2026-02-10 12:15:24 +0530

    Update graph.js

--- graph.js
+++ graph.js
@@ -30,4 +30,67 @@ var cy = cytoscape({
         'text-wrap': 'wrap',
         'text-max-width': '90px',
         'background-color': '#4f46e5',
-        'color': '
+        'color': '#fff',
+        'font-size': '11px',
+        'font-weight': 'bold'
+      }
+    },
+    {
+      selector: '#MARA',
+      style: {
+        'background-color': '#dc2626',
+        'font-size': '12px'
+      }
+    },
+    {
+      selector: '.selected',
+      style: {
+        'border-width': 3,
+        'border-color': '#facc15',
+        'background-color': '#1d4ed8'
+      }
+    },
+    {
+      selector: 'edge',
+      style: {
+        'label': 'data(label)',
+        'curve-style': 'bezier',
+        'target-arrow-shape': 'triangle',
+        'line-color': '#9ca3af',
+        'target-arrow-color': '#9ca3af',
+        'font-size': '9px',
+        'color': '#111',
+        'text-background-color': '#fff',
+        'text-background-opacity': 1,
+        'text-background-padding': '3px'
+      }
+    }
+  ],
+
+  layout: {
+    name: 'cose',
+    animate: true
+  }
+});
+
+
+// Hover interaction
+cy.on('mouseover', 'node', function (evt) {
+  evt.target.animate(
+    { style: { 'transform': 'scale(1.1)' } },
+    { duration: 150 }
+  );
+});
+
+cy.on('mouseout', 'node', function (evt) {
+  evt.target.animate(
+    { style: { 'transform': 'scale(1)' } },
+    { duration: 150 }
+  );
+});
+
+// Click selection
+cy.on('tap', 'node', function (evt) {
+  cy.nodes().removeClass('selected');
+  evt.target.addClass('selected');
+});
